e4d57d860906654d7129dee7ffe83fc187650b9b
Jonny van Beek <jb@plxinc.com>
2022-12-06 18:21:31 +0000

    Day 1 Part 2 complete

---
+++ Day-1-Part-2.py
@@ -0,0 +1,23 @@
+
+def OrderdedInsert(insertList, value):
+    # Find the palce to insert
+    for i, arrayVal in enumerate(insertList):
+        if value > arrayVal:
+            insertList.insert(i, value)
+            insertList.pop(len(insertList) - 1)
+            break
+        
+if __name__ == "__main__":
+    with open(r"./Inputs/Day-1-Part-1-Input.txt") as f:
+        maxCalories = [0, 0, 0]
+        thisElfCalories = 0
+        for line in f:
+            line = line.replace("\n", "")
+            if not line:
+                OrderdedInsert(maxCalories, thisElfCalories)
+                thisElfCalories = 0
+            else:
+                thisElfCalories += int(line)
+
+        print(maxCalories)
+        print(sum(maxCalories))
