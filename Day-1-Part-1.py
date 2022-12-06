36a14e22b4c48c97ce8d637986dbf0d3f328ff65
Jonny van Beek <jb@plxinc.com>
2022-12-06 18:03:14 +0000

    Remove a semicolon (force of habit...)

--- Day-1-Part-1.py
+++ Day-1-Part-1.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     with open(r"./Inputs/Day-1-Part-1-Input.txt") as f:
         maxCalories = 0
-        thisElfCalories = 0;
+        thisElfCalories = 0
         for line in f:
             line = line.replace("\n", "")
             if not line:
