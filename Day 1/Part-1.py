7af4aaf8eb8af978fb28995f10f9bc6e86bcb49c
Jonny van Beek <jb@plxinc.com>
2022-12-07 15:15:53 +0000

    Fix code for updated paths

--- Day 1/Part-1.py
+++ Day 1/Part-1.py
@@ -1,6 +1,6 @@
 
 if __name__ == "__main__":
-    with open(r"./Inputs/Day-1-Part-1-Input.txt") as f:
+    with open("./Day 1/Day-1-Input.txt") as f:
         maxCalories = 0
         thisElfCalories = 0
         for line in f:
