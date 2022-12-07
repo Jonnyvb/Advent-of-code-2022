1223497c97693e3790cfed4c8a1b1c63f9540df5
Jonny van Beek <jb@plxinc.com>
2022-12-07 15:12:00 +0000

    Rearrange directory setup

---
+++ Day 1/Part-1.py
@@ -0,0 +1,16 @@
+
+if __name__ == "__main__":
+    with open(r"./Inputs/Day-1-Part-1-Input.txt") as f:
+        maxCalories = 0
+        thisElfCalories = 0
+        for line in f:
+            line = line.replace("\n", "")
+            if not line:
+                if thisElfCalories > maxCalories:
+                    maxCalories = thisElfCalories
+
+                thisElfCalories = 0
+            else:
+                thisElfCalories += int(line)
+
+        print(maxCalories)
